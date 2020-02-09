1f91676f9efe1ed70658c2a6980a05242e8f1542
lpdft <772346730@qq.com>
2020-02-09 13:02:09 +0800

    [ruyue] feat: create Eagle class

---
+++ 005_extends/src/main/java/Eagle.java
@@ -0,0 +1,22 @@
+public class Eagle extends Bird {
+    private String[] food;
+
+    public Eagle() {}
+
+    public Eagle(int age, double weight, double flySpeed, String[] food) {
+        super(age, weight, flySpeed);
+        this.food = food;
+    }
+
+    public String[] getFood() {
+        return food;
+    }
+
+    public void setFood(String[] food) {
+        this.food = food;
+    }
+
+    public void eatWay() {
+
+    }
+}
